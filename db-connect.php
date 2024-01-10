0c962fe0be68ecfa822bcf35951ce30e46ee9c56
hakutojp <2201365@s.asojuku.ac.jp>
2024-01-10 13:43:33 +0900

    db接続

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517356-final';
+    const USER = 'LAA1517356';
+    const PASS = 'Pass1208';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
